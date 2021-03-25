64f73e8e9fb3bd9a804a1afe51e6d19761744690
eh7644 <78935784+eh7644@users.noreply.github.com>
2021-03-24 19:58:08 -0700

    New menu file, required in script.user.js later.
    Do not install this seperately, make sure to install script.js as it requires these automatically.

---
+++ menu.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         TheZombsProject: Menu
+// @namespace    https://github.com/eh7644/thezombsproject
+// @version      1.0.0
+// @description  Script menu, script is installed at https://github.com/eh7644/thezombsproject/menu.user.js
+// @author       thezombsproject
+// @match        zombs.io
+// @require      https://github.com/eh7644/thezombsproject/import_styles.js
+// @grant        none
+// ==/UserScript==
+
+
+const menuHTML = 
+
+`
+<h1 class="menu menu-h1 menu-h1-title">./theZombsProject</h1>
+<a href="https://github.com/eh7644/thezombsproject">Contribute</a>
+<style>${window.tzp_imports.style}</style>
+
+`
+
