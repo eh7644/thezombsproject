5a92148e63f6078d6687298fa6cbf93610bce8f5
eh7644 <78935784+eh7644@users.noreply.github.com>
2021-03-24 20:11:57 -0700

    Added @require tags to menu.user.js

--- script.user.js
+++ script.user.js
@@ -5,6 +5,7 @@
 // @description  Instructions at the GitHub page on how to install and use it, aka https://github.com/eh7644/thezombsproject/blob/main/README.md
 // @author       thezombsproject
 // @match        zombs.io
+// @require      https://github.com/eh7644/thezombsproject/raw/main/menu.user.js
 // @grant        none
 // ==/UserScript==
 
