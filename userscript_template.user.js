3c36162eb1c1444429714b9e08a1cd0590a2cb2e
eh7644 <78935784+eh7644@users.noreply.github.com>
2021-03-24 20:24:43 -0700

    Create userscript_template.user.js

---
+++ userscript_template.user.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name         TheZombsProject: Template
+// @namespace    https://github.com/eh7644/thezombsproject/
+// @version      1.0.0
+// @description  This is just a template we can copy to other scripts.
+// @author       thezombsproject
+// @match        zombs
+// @grant        none
+// ==/UserScript==
