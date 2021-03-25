9374a134bd09fb25f1b85e3b9f4ec1ce552d6cdc
eh7644 <78935784+eh7644@users.noreply.github.com>
2021-03-24 20:07:47 -0700

    A

---
+++ script.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         TheZombsProject
+// @namespace    https://github.com/eh7644/thezombsproject/
+// @version      0.1
+// @description  Instructions at the GitHub page on how to install and use it, aka https://github.com/eh7644/thezombsproject/blob/main/README.md
+// @author       thezombsproject
+// @match        zombs.io
+// @grant        none
+// ==/UserScript==
+
+// To-Do: Import other files from GitHub, as now they are not working.
