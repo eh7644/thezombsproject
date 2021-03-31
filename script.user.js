0686e2ee7a3f94dd4788a1d166089185505dda5e
eh7644 <78935784+eh7644@users.noreply.github.com>
2021-03-31 07:24:30 -0700

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         TheZombsProject
 // @namespace    https://github.com/eh7644/thezombsproject/
-// @version      1.7.2
+// @version      1.7.9
 // @description  Instructions at the GitHub page on how to install and use it, aka https://github.com/eh7644/thezombsproject/blob/main/README.md
 // @author       thezombsproject
 // @match        zombs.io
