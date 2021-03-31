4f51cdadd7e29b847bed450f6124c1483c38e715
eh7644 <78935784+eh7644@users.noreply.github.com>
2021-03-31 07:09:02 -0700

    Update menu.user.js

--- menu.user.js
+++ menu.user.js
@@ -269,8 +269,9 @@ document.getElementsByClassName("hud-settings-grid")[0].innerHTML = `
 <button class=\"btn btn-greenn\"style=\"width: 45%;\" id="sellminesbtn">Sell Gold Mines?</button>
 <button class=\"btn btn-greenn\"style=\"width: 45%;\" id="sellharvsbtn">Sell Harvesters?</button>
 <hr />
-<div style="text-align:initial;"><label>Upgrade all except towers? </label> <input type="checkbox" id="upallbutchk" /></div>
+<div style="text-align:initial;"><label>Upgrade all except towers? </label><input type="checkbox" id="upallbutchk" /></div>
 <hr />
+<br />
 <button class=\"btn btn-orangee\"style=\"width: 45%;\" id="upwallsbtn">Upgrade Walls?</button>
 <button class=\"btn btn-orangee\"style=\"width: 45%;\" id="updoorsbtn">Upgrade Doors?</button>
 <button class=\"btn btn-orangee\"style=\"width: 45%;\" id="uptrapsbtn">UpgradeSlowTraps?</button>
