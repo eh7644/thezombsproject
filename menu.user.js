03a705b154ea009448c031cdf8882e53d33b288e
the7639487 <81666886+the7639487@users.noreply.github.com>
2021-04-08 08:53:38 +0400

    added main y features (find players by name, get highest waves, get highest scores)

--- menu.user.js
+++ menu.user.js
@@ -289,8 +289,14 @@ document.getElementsByClassName("hud-settings-grid")[0].innerHTML = `
 <button class=\"btn btn-orangee\"style=\"width: 45%;\" id="upharvsbtn">Upgrade Harvesters?</button>
 <hr />
 <h3>Scan Players (main y features)</h3>
-<input class="scanpplinput" value="Player">
+<input class="scanpplinput" value="Player" type="text" placeholder="name">
 <button class="scanpplbutton">Scan?</button>
+<br>
+<input class="scanpplinput2" value="1000" type="number" placeholder="highestwave">
+<button class="highestwavebutton">Get hws?</button>
+<br>
+<input class="scanpplinput3" value="1000000000" type="number" placeholder="highestscore">
+<button class="highestscorebutton">Get hss?</button>
 <p class = "idk"></p>
 <hr />
 <h3>Miscellaneous</h3>
